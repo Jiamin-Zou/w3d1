617e008d41a8d46b8d2d22748cb14bbb947bb0a1
Mohammad Naqvi <mohammadnaqvi25@gmail.com>
2023-03-20 16:32:08 -0400

    up to my_flatten

--- practice-for-ch-ruby-enumerables-long-practice-main/enumerables_array.rb
+++ practice-for-ch-ruby-enumerables-long-practice-main/enumerables_array.rb
@@ -10,7 +10,34 @@ class Array
     end
 
     def my_select(&blk)
-        
+        new_arr = []
+        my_each { |i| new_arr << i if blk.call(i) }
+        return new_arr
+    end
+
+    def my_reject(&blk)
+        new_arr = []
+        my_each { |i| new_arr << i if !blk.call(i) }
+        return new_arr
+    end
+
+    def my_any?(&blk)
+        my_each { |i| return true if blk.call(i) }
+        return false
+    end
+
+    def my_all?(&blk)
+        my_each { |i| return false if !blk.call(i) }
+        return true
+    end
+
+    def my_flatten(arr)
+        return [arr] if !arr.is_a?(Array)
+        new_arr = []
+        arr.each do |ele|
+            new_arr += my_flatten(ele)
+        end
+        return new_arr
     end
 
 end
